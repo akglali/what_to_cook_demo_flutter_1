dae481a131b385c82ca46e7d9c88354d859e0fe8
akglali <akgol97_@hotmail.com>
2023-01-15 22:53:53 -0500

    prevent device orientation changes and force portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:what_to_cook_demo_flutter_1/providers/themes_provider.dart';
-import 'package:what_to_cook_demo_flutter_1/services/sign_in_screen.dart';
+import 'package:what_to_cook_demo_flutter_1/screens/home_screen.dart';
 import 'package:what_to_cook_demo_flutter_1/utils/themes.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
@@ -13,15 +14,21 @@ class MyApp extends ConsumerWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
-    final themeModeState= ref.watch(themesProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    // prevent device orientation changes and force portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    final themeModeState = ref.watch(themesProvider);
     return MaterialApp(
       theme: MyThemes.lightTheme,
       darkTheme: MyThemes.darkTheme,
       themeMode: themeModeState,
       //I don't like debug banner so I disable it
       debugShowCheckedModeBanner: false,
-      home: SignInScreen(),
+      home: HomeScreen(),
     );
   }
 }
